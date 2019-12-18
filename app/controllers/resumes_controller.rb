152d7f641e876cbd4cc4f85eaab19f85f941aea9
Last_Vegas <AC-130a@yandex.ru>
2019-12-18 16:37:28 +0300

    Integrated new and create actioncs for resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -2,4 +2,21 @@ class ResumesController < ApplicationController
   def index
     @resume_item = Resume.all
   end
+
+  def new
+    @resume_item = Resume.new
+  end
+
+  def create
+    @resume_item = Resume.new(params.require(:resume).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @resume_item.save
+        format.html { redirect_to resumes_path, notice: 'Your portfolio item created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
