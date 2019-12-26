7c9eef848bb436b44be2253ec0ab6c9a32427f7c
Last_Vegas <AC-130a@yandex.ru>
2019-12-26 13:58:33 +0300

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :resumes
+  resources :resumes, except: [:show]
+  get 'resume/:id', to: 'resumes#show', as: 'resume_show'
 
   get 'about' , to: 'pages#about'
   get 'contact' , to: 'pages#contact'
