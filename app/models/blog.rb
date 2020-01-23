3407e755873ffa650e897bf28e8717e337d12b06
Last_Vegas <AC-130a@yandex.ru>
2020-01-23 12:05:13 +0300

    Implemented validations for blogs, skills and resume items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  enum status:{ draft: 0, published: 1 }
-  extend FriendlyId
-  friendly_id :title, use: :slugged
+  enum status:{ draft: 0, published: 1 } #возможность выбрать блог в драфте или публикации
+  extend FriendlyId #читабельная строка в браузере
+  friendly_id :title, use: :slugged #читабельная строка в браузере
+
+  validates_presence_of :title, :body #защита от создания пустого блога
 end
