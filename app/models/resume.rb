805574c71658ee702e02402d19a5436a90eaf9de
Last_Vegas <AC-130a@yandex.ru>
2020-01-24 20:25:30 +0300

    Inplemented image defaults for resume item

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -6,4 +6,11 @@ class Resume < ApplicationRecord
   end
 
   scope :ruby_on_rails_resume_items, -> {where(subtitle: 'Ruby on Rails')}  #функция вызывающия все айтемы с названием Ruby on Rails
+
+  after_initialize :set_defaults #автоматическое выполнение функции set_defaults при создании resume_item
+
+  def set_defaults #функция дает при создании resume_item дефолтные картинки
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/300x200"
+  end
 end
