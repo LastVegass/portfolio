3678092e68e1d827b8b5cc057c8fbd315ab8bfb2
Last_Vegas <AC-130a@yandex.ru>
2020-01-24 18:11:02 +0300

    Integrated database relationship between topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,6 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged #читабельная строка в браузере
 
   validates_presence_of :title, :body #защита от создания пустого блога
+
+  belongs_to :topic
 end
