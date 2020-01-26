62aaf61ca5d2d3e88b8e11ecf928ae565def2198
Last_Vegas <AC-130a@yandex.ru>
2020-01-26 17:17:17 +0300

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,3 +47,11 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Resume.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
