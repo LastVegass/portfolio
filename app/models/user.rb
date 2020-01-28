fca7a90ecd810f1e0d7c9043c92fcd9ba33e0b7a
Last_Vegas <AC-130a@yandex.ru>
2020-01-28 22:18:42 +0300

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   #:omniauthable - регистрация  с помощью других сервисов например соц сетей вк фейсбук и тд
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
