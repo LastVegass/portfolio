d106af7436605c4aee49ab32ed7055bf67f649ed
Last_Vegas <AC-130a@yandex.ru>
2020-02-04 16:50:54 +0300

    Fix session tracker for source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
 
   before_action :set_source
-  def set_source #для отслеживания перехода из соц сетей
+  def set_source #для отслеживания источникка перехода на сайт
     session[:source] = params[:q] if params[:q]
   end
 end
