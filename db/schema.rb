5bc0bdd7898d33aebc178b846ecfa47af9adeb76
Last_Vegas <AC-130a@yandex.ru>
2020-03-20 18:47:28 +0300

    Added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_191611) do
+ActiveRecord::Schema.define(version: 2020_03_20_154448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_topics", id: false, force: :cascade do |t|
+    t.bigint "blog_id", null: false
+    t.bigint "topic_id", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -85,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_191611) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
